2fc968c6d50d5e7278e0495ea71f198e5c6ac38d
= <=>
2021-11-20 15:00:27 +0100

    :fire: Initial commit

---
+++ source/components/app-container.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { NativeBaseProvider } from 'native-base'
+
+type Props = {
+  children: React.ReactNode
+} 
